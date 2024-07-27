e8320a9df312d7056deef379351b3f6877536db5
Philippe Marschall <philippe.marschall@gmail.com>
2024-07-27 16:18:05 +0200

    add initial size

--- object-size-agent/src/main/c/objectsize.c
+++ object-size-agent/src/main/c/objectsize.c
@@ -46,27 +46,37 @@ JNIEXPORT jboolean JNICALL Java_com_github_marschall_objectsize_JvmtiObjectSizeC
   return gdata->supported;
 }
 
-JNIEXPORT jlong JNICALL Java_com_github_marschall_objectsize_JvmtiObjectSizeCalculator_getObjectSize0(JNIEnv *env, jclass clazz, jobject object, jclass exceptionClass) {
-  jlong total_size = 0;
-  jvmtiEnv *jvmti = gdata->jvmti;
-  jint niError;
+jint jvmti_call_failed(JNIEnv *env, jvmtiError tiError, char* failedFunctionName, jclass exceptionClass) {
   char* message;
   int printCode;
 
-  jvmtiError error = (*jvmti)->IterateOverObjectsReachableFromObject(jvmti,
-          object,
-          &VisitObjectCallback,
-          &total_size);
-  if (error != JVMTI_ERROR_NONE) {
-    printCode = asprintf(&message, "IterateOverObjectsReachableFromObject failed with code: %d", error);
-    if (printCode != 0) {
-     // we could create an exception message
-     niError = (*env)->ThrowNew(env, exceptionClass, message);
-     free(message);
-    } else {
-     // we could not format an exception message
-     niError = (*env)->ThrowNew(env, exceptionClass, "IterateOverObjectsReachableFromObject failed");
-    }
+  printCode = asprintf(&message, "%s failed with code: %d", failedFunctionName, tiError);
+  if (printCode != 0) {
+   // we could create an exception message
+   return (*env)->ThrowNew(env, exceptionClass, message);
+   free(message);
+  } else {
+   // we could not format an exception message
+   return (*env)->ThrowNew(env, exceptionClass, "JVMTI call failed");
+  }
+}
+
+JNIEXPORT jlong JNICALL Java_com_github_marschall_objectsize_JvmtiObjectSizeCalculator_getObjectSize0(JNIEnv *env, jclass clazz, jobject object, jclass exceptionClass) {
+  jlong total_size = 0;
+  jvmtiEnv *jvmti = gdata->jvmti;
+  jvmtiError tiError;
+
+  // initialize the the direct size
+  tiError = (*jvmti)->GetObjectSize(jvmti, object, &total_size);
+  if (tiError != JVMTI_ERROR_NONE) {
+    jvmti_call_failed(env, tiError, "GetObjectSize", exceptionClass);
+    return -1;
+  }
+
+  // add the sizes of the reachable objects
+  tiError = (*jvmti)->IterateOverObjectsReachableFromObject(jvmti, object, &VisitObjectCallback, &total_size);
+  if (tiError != JVMTI_ERROR_NONE) {
+    jvmti_call_failed(env, tiError, "IterateOverObjectsReachableFromObject", exceptionClass);
     return -1;
   }
   return total_size;
@@ -114,10 +124,12 @@ int enable_tag_objects(jvmtiEnv* jvmti, jboolean* supported) {
     (*supported) = JNI_FALSE;
     return tiError;
   }
+  // successfully added capability
+  (*supported) = JNI_TRUE;
   return 0;
 }
 
-JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM* vm, char *options, void *reserved) {
+JNIEXPORT jint JNICALL Agent_OnLoad(JavaVM *vm, char *options, void *reserved) {
   jvmtiEnv* jvmti;
   jint niError;
   jboolean supported;
