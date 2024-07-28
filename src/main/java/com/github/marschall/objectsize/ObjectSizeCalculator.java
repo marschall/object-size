ea50511b0c0017357b9d7dbf09c1128cd9238a11
Philippe Marschall <philippe.marschall@gmail.com>
2024-07-28 16:14:51 +0200

    improve javadoc

--- src/main/java/com/github/marschall/objectsize/ObjectSizeCalculator.java
+++ src/main/java/com/github/marschall/objectsize/ObjectSizeCalculator.java
@@ -1,9 +1,27 @@
 package com.github.marschall.objectsize;
 
+/**
+ * Functions to get object sizes.
+ */
 public interface ObjectSizeCalculator {
 
+  /**
+   * Computes the size of an object and all objects reachable from it.
+   * 
+   * @param o the object to start the size calclation
+   * @return the size of {@code o} and all objects reachable from it in bytes,
+   *         {@code 0} if {@code o} is {@code null}
+   */
   long getObjectSize(Object o);
 
+  /**
+   * Returns the sole instance of {@link ObjectSizeCalculator}.
+   * <p>
+   * If computing object sizes is not suported an instance that always throws {@link UnsupportedOperationException}
+   * is returned.
+   * 
+   * @return the sole instance of {@link ObjectSizeCalculator}, never {@code null}
+   */
   static ObjectSizeCalculator getInstance() {
     return InstanceHolder.INSTANCE;
   }
